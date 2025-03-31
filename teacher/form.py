17fc14f6b364c518edf739a07fef460ac28a646d
awais3344 <awaisalishah02@gmail.com>
2025-03-31 12:24:00 +0500

    Blog 2

--- teacher/form.py
+++ teacher/form.py
@@ -2,5 +2,5 @@ from django import forms
 
 class TeachersForm(forms.Form):
     name = forms.CharField(max_length=50)
-    email=forms.EmailField(label="Your Email" , label_suffix=" = " , required=False)
-    phone_number=forms.IntegerField(min_value=11, label="Your Phone Number ( +92 )")
+    email=forms.EmailField(label="Your Email" , label_suffix=" = " , required=False , help_text="We accept email of google only")
+    phone_number=forms.IntegerField(min_value=11, label="Your Phone Number ( +92 )", )
