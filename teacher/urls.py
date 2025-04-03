054d61711f973789fe6c6e36032d924d4c1efa58
awais3344 <awaisalishah02@gmail.com>
2025-04-03 10:54:54 +0500

    Blog 2

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Teacher,thanks
+from .views import Teacher_view,thanks
 
 urlpatterns = [
     
-    path("teacher/",Teacher ,name='teacher' ),
+    path("teacher/",Teacher_view ,name='teacher' ),
     path("thank-you/",thanks ,name='thanks' )
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from .form import TeachersForm
 from django.http import HttpResponseRedirect , HttpResponse
+from .models import Teacher
+from .form import TeachersForm
 
 # Create your views here.
-def Teacher(request):
+def Teacher_view(request):
 
     if request.method == 'POST':
 
         form=TeachersForm(request.POST)
         if form.is_valid():
-            # form.cleaned_data()
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            phone_number=form.cleaned_data['phone_number']
+            bio=form.cleaned_data['Bio']
+            teacher=Teacher.objects.create(name=name,email=email,phone_number=phone_number,bio=bio)
 
             return HttpResponseRedirect("/thank-you/")
     else:
         form=TeachersForm()
     context={"form":form}
     return render(request,"teacher/form.html",context)
+
+
+
+
+def all_data(request):
+    techer
 
 
 def thanks(request):
