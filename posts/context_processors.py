aa0bafacb3145cb2d1b28127be67ef4795d74d8d
awais3344 <awaisalishah02@gmail.com>
2025-04-04 20:53:50 +0500

    Cookies

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -14,4 +14,6 @@ def post(request):
     },
 ]
     
+    
+    
     return {"posts":posts}
--- student/views.py
+++ student/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def set(request):
+
+    response=HttpResponse('<h1> Set <h1>')
+    response.set_cookie("theme","dark")
+
+    return response
