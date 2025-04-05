e67a134c4df3f07ee5e5441c0499e32fde2d935d
awais3344 <awaisalishah02@gmail.com>
2025-04-05 22:50:06 +0500

    Cookies

--- student/middleware.py
+++ student/middleware.py
@@ -23,20 +23,40 @@
 class CustomClassMiddleware:
 
     def __init__(self, get_response):
+
         self.get_response=get_response
         
 
         print("one time config")
 
-    def __call__(self,request):
-
+    def __call__(self, request):
         print("Before View Called")
+        print(f"Request path: {request.path}")
 
-        response=self.get_response(request)
+        if request.path.strip() == '/set/':
+            print("Set view is called")
+
+        # Call the view (or the next middleware)
+        response = self.get_response(request)
 
         print("After View Called")
 
-
         return response
+
+    # def __call__(self,request):
+
+    #     print("Before View Called")
+
+    #     print(request.path)
+
+    #     if request.path == '/student/set/':
+    #         print("set vire is called")
+
+    #     response=self.get_response(request)
+
+    #     print("After View Called")
+
+
+    #     return response
     
    
