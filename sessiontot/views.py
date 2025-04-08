4528ca194c92afd4adb35c9c42c7c9fec19d7be0
awais3344 <awaisalishah02@gmail.com>
2025-04-09 00:02:53 +0500

    Cookies

--- sessiontot/views.py
+++ sessiontot/views.py
@@ -4,21 +4,27 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def set(request):
-
-    
-    city = request.session['city']="Mardan"
+    request.session['name']={"name1":"Awais","name2":"Sudais"}
+    request.session['city']="Mardan"
 
 
     return HttpResponse("<h1>Session Addition</h1>")
 
 def get(request):
-
+    name=request.session["name"]
     city = request.session['city']
 
-    return HttpResponse(f"<h1>Getting Sesssion Contents </h1> <br> {city}")
+    return HttpResponse(f"<h1>Getting Sesssion Contents </h1> <br> {name}")
 
 def delete(request):
 
-    request.session.flush()
-    del request.session['city']
+    request.session.flush()   #use if you want to also delete session from datase
+    del request.session['city']  #use if you wanna just delete from session(browser) just
     return HttpResponse("<h1>deleted</h1>")
+
+def update(request):
+
+    request.session['name']['name1']="Sudais"
+
+    request.session.mofified=True
+    return HttpResponse("<h1>Getting Updated </h1>")
