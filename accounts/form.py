f7ecb5769ed303858c5aaa48baa7619f5a20a5ce
awais3344 <awaisalishah02@gmail.com>
2025-04-09 12:32:50 +0500

    Accounts Section Addition

--- accounts/form.py
+++ accounts/form.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User
 
 class RegistartionForm(UserCreationForm):
 
-    first_name=forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder":"e.g Awais"}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={"placeholder":"e.g Awais"}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control","placeholder":"e.g Ali"}))
-    Email
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control","placeholder":"xyz123@gmail.com"}))
 
     class Meta:
         model = User
         fields = ["username","first_name",]
         widgets={
-            "username":forms.TextInput(attrs={"placeholder":"e.g awais8900" , "class":"form-control"})
+            "username":forms.TextInput(attrs={"placeholder":"e.g awais8900" , "class":"form-control"}),
+            "password1":forms.PasswordInput(attrs={"placeholder":"e.g awais8900" , "class":"form-control"}),
+            "password2":forms.PasswordInput(attrs={"placeholder":"e.g awais8900" , "class":"form-control"}),
+
         }
