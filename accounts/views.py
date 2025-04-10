da0e69a64baeb1219c8d510317eb4e4df183370f
awais3344 <awaisalishah02@gmail.com>
2025-04-10 12:25:22 +0500

    Accounts Section Addition

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,authenticate
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .form import RegistartionForm
@@ -19,3 +20,20 @@ def register(request):
         form=RegistartionForm()
     context={"form":form}
     return render(request,"accounts/home.html",context)
+
+def auth_login(request):
+    if request.method == 'POST':
+        form=AuthenticationForm(request=request,data=request.POST)
+
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data["password"]
+            user=authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                home = reverse("home")
+                return HttpResponseRedirect(home)
+    else:
+        form=AuthenticationForm()
+    context={"form":form}
+    return render(request,"accounts/login.html",context)
