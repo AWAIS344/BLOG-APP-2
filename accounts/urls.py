b945c89a111c0c9e04b7541296bbb6b0d7834184
awais3344 <awaisalishah02@gmail.com>
2025-04-12 01:03:48 +0500

    Accounts Section Addition

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView,LogoutView
+from.form import LoginFrom
 
 urlpatterns = [
     path("register/",views.register,name="register"),
-    path("login/",LoginView.as_view(),name="auth-login"),
+    path("login/",LoginView.as_view(template_name="accounts/login.html",authentication_form=LoginFrom),name="auth-login"),
     path("logout/",LogoutView.as_view(),name="auht-logout"),
 
 ]
