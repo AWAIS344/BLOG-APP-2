956cb5e612b996b2c8967ee30267a64aeed81af0
awais3344 <awaisalishah02@gmail.com>
2025-04-11 10:19:08 +0500

    Accounts Section Addition

--- accounts/form.py
+++ accounts/form.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField
 from django import forms
 from django.contrib.auth.models import User
 
@@ -18,5 +18,5 @@ class RegistartionForm(UserCreationForm):
         }
 
 class LoginFrom(AuthenticationForm):
-    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Your Username Here"}))
+    username=UsernameField(widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Your Username Here","autofocus":True}))
     password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"Your Username Here"}))
