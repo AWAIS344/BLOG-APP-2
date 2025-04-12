0d0e912a6311541d32efd412f7f61468a32c06ac
awais3344 <awaisalishah02@gmail.com>
2025-04-12 22:40:51 +0500

    Accounts Section Addition

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,5 +130,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+LOGIN_REDIRECT_URL = reverse_lazy('home')  # Replace 'home' with your named URL
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- student/views.py
+++ student/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import logout
 from django.template.response import TemplateResponse
 
 # Create your views here.    
 
 
 def set(request):
-    login=reverse("auth-login")
+    login=reverse("login")
     if not request.user.is_authenticated:
         return HttpResponseRedirect(login)
     else:
 
+        if request.method == 'POST':
+            logout(request)
+            HttpResponseRedirect(login)
         # x=1/0
         response=TemplateResponse(request,"student/home.html",{})
         response.set_cookie("theme","dark")
