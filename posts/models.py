afe1a5c20639949b545c626a0d364027d32a65da
awais3344 <awaisalishah02@gmail.com>
2025-04-15 14:43:48 -0300

    Accounts Section Addition

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Tag(models.Model):
+    name=models.CharField()
+
+
 class Posts(models.Model):
 
     post_title=models.CharField(max_length=60)
     post_content=models.TextField()
     updated_at =models.DateTimeField(auto_now=True)
+    tag=models.ManyToManyField(Tag)
+
+class Comment(models.Model):
+    commment=models.TextField()
+    
+
